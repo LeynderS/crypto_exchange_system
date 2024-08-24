3030dcc7ac9a41b66d0c3b304d3ce66309f2ce99
Leynder <leynder.elian@gmail.com>
2024-08-24 13:10:55 -0500

    Email Validation UserService

--- src/main/java/com/globant/service/UserService.java
+++ src/main/java/com/globant/service/UserService.java
@@ -2,8 +2,13 @@ package com.globant.service;
 
 import com.globant.models.User;
 
+import java.util.regex.Pattern;
+
 public class UserService {
     private final InMemoryUserRepository userRepository;
+    private static final String EMAIL_REGEX = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+    private static final Pattern EMAIL_PATTERN = Pattern.compile(EMAIL_REGEX);
+
     private User currentUser;
     public UserService(InMemoryUserRepository userRepository) {
         this.userRepository = userRepository;
@@ -11,6 +16,7 @@ public class UserService {
     }
 
     public void registerUser(String name, String email, String password) {
+        validateEmail(email);
         if(userRepository.getUserByEmail(email) != null){
             throw new EmailInUseException();
         }
@@ -32,5 +38,9 @@ public class UserService {
         if (currentUser != null) currentUser = null;
     }
 
-
+    public void validateEmail(String email) {
+        if (!EMAIL_PATTERN.matcher(email).matches()) {
+            throw new InvalidCredentialsException();
+        }
+    }
 }
